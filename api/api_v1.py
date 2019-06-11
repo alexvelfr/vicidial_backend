946b51b6e2bc0bdaf0d01d6eb87733dff3ad1d7a
velfr <kudrushow@gmail.com>
2019-06-11 17:59:49 +0300

    add auth header

--- api/api_v1.py
+++ api/api_v1.py
@@ -1,26 +1,33 @@
+import os
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
-
 from .utils import make_request_to_1c
 
 api = Blueprint('api', __name__)
-allowed_actions = ('send_sms', 'get_payment_requsits', 'get_main_info', 'get_loan_info', 'get_ticket_info')
+allowed_actions = ('send_sms', 'get_payment_requsits', 'get_main_info', 'get_loan_info', 'get_ticket_info',
+                   'get_detail_loan', 'get_detail_ticket')
 
 
 @api.route('/ivr', methods=['GET', 'POST'])
 @cross_origin()
 def get_ivr_info():
+    auth_key = request.headers.get('X-Auth-Key', '')
+    if auth_key != os.environ.get('AUTH_KEY'):
+        return jsonify(error='Not authenticated')
     phone = request.args.get('phone')
     send_sms = bool(request.args.get('sendsms', False))
     if phone:
         response = make_request_to_1c('ivr', {'phone': phone, 'send_sms': send_sms})
         return jsonify(response)
-    return jsonify({'error': 'phone is undefined'})
+    return jsonify(error='phone is undefined')
 
 
 @api.route('/vicidial/<action>', methods=['POST'])
 @cross_origin()
 def vicidial_handler(action):
+    auth_key = request.headers.get('X-Auth-Key', '')
+    if auth_key != os.environ.get('AUTH_KEY'):
+        return jsonify(error='Not authenticated')
     try:
         data = request.get_json()
     except:
