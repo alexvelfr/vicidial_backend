15059cba8ceb10845533bfe185b564e14f7433ca
velfr <kudrushow@gmail.com>
2019-06-21 14:05:01 +0300

    add new endpoint add DEBUG mode

--- api/utils.py
+++ api/utils.py
@@ -5,13 +5,16 @@ import os
 
 HEADERS = {'Authorization': 'Basic {}'.format(os.environ.get('AUTH_1C'))}
 BASE_URL_1C = os.environ.get('BASE_URL_1C')
-
+DEBUG = os.environ.get('DEBUG', '') == 'True'
 
 def make_request_to_1c(resourse, req):
     url = BASE_URL_1C + resourse
     try:
         rt = requests.post(url, data=json.dumps(req, ensure_ascii=False).encode('utf-8'), headers=HEADERS,
                            verify=False).text
+        if DEBUG:
+            print(req)
+            print(rt)
         rt = json.loads(rt, encoding='utf-8-sig')
     except:
         rt = {'error': 'connection error'}
